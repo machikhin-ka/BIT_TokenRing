01b3d7ff222a6b7fe813e6255dde6ab8ff36f296
machikhin-ka <machihin.ka@phystech.edu>
2021-05-16 16:21:40 +0300

    add single

--- src/main/java/tokenring/queue/QueueTokenRing.java
+++ src/main/java/tokenring/queue/QueueTokenRing.java
@@ -1,27 +1,21 @@
 package tokenring.queue;
 
-import tokenring.*;
-import tokenring.consumers.LatencyConsumerDecorator;
-import tokenring.consumers.ThroughputConsumerDecorator;
+import tokenring.Medium;
+import tokenring.Token;
+import tokenring.TokenRing;
+import tokenring.TokenRingImpl;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class QueueTokenRing implements TokenRing {
-	private final List<Medium> mediums;
-	private final List<Node> nodes;
-	private final List<Thread> nodeThreads;
-	private final Consumer<Token> consumer;
-	private int tokenCount;
+	private final TokenRing tokenRing;
 
 	public QueueTokenRing(int nodeCount, int capacity, Consumer<Token> consumer) {
-		this.consumer = consumer;
-		mediums = createMedium(nodeCount, capacity);
-		nodes = createNodes();
-		nodeThreads = createNodeThreads();
+		List<Medium> mediums = createMedium(nodeCount, capacity);
+		tokenRing = new TokenRingImpl(mediums, consumer);
 	}
 
 	private List<Medium> createMedium(int nodeCount, int capacity) {
@@ -30,73 +24,33 @@ public class QueueTokenRing implements TokenRing {
 				.collect(Collectors.toList());
 	}
 
-	private List<Node> createNodes() {
-		List<Node> nodes = new ArrayList<>(mediums.size());
-		for (int i = 0; i < mediums.size(); i++) {
-			int nextId = (i + 1) % mediums.size();
-			nodes.add(new NodeImpl(i, consumer, mediums.get(nextId)));
-		}
-		Node node = nodes.get(0);
-		Consumer<Token> consumer = node.getConsumer();
-		node.setConsumer(new LatencyConsumerDecorator(consumer));
-		node = nodes.get(nodes.size() - 1);
-		consumer = node.getConsumer();
-		node.setConsumer(new ThroughputConsumerDecorator(consumer));
-		return nodes;
-	}
-
-	private List<Thread> createNodeThreads() {
-		return nodes.stream()
-				.map(n -> new NodeRunnable(n, mediums.get(n.getId())))
-				.map(Thread::new)
-				.collect(Collectors.toList());
-	}
-
 	@Override
 	public void start() {
-		nodeThreads.forEach(Thread::start);
+		tokenRing.start();
 	}
 
 	@Override
 	public void stop() {
-		nodeThreads.stream()
-				.peek(Thread::interrupt)
-				.forEach(thread -> {
-					try {
-						thread.join();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				});
+		tokenRing.stop();
 	}
 
 	@Override
 	public void sendTokens(int n) {
-		for (int i = 0; i < n; i++) {
-			sendToken();
-		}
+		tokenRing.sendTokens(n);
 	}
 
 	@Override
 	public void sendToken() {
-		try {
-			int from = tokenCount;
-			mediums.get(from).push(new Token());
-			tokenCount = (tokenCount + 1) % mediums.size();
-		} catch (InterruptedException ignore) {
-		}
+		tokenRing.sendToken();
 	}
-
 
 	@Override
 	public long[] getLatencies() {
-		LatencyConsumerDecorator consumer = (LatencyConsumerDecorator) nodes.get(0).getConsumer();
-		return consumer.getLatencies();
+		return tokenRing.getLatencies();
 	}
 
 	@Override
 	public long[] getThroughput() {
-		ThroughputConsumerDecorator consumer = (ThroughputConsumerDecorator) nodes.get(nodes.size() - 1).getConsumer();
-		return consumer.getThroughputs();
+		return tokenRing.getThroughput();
 	}
 }
--- src/test/java/tokenring/queue/QueueTokenRingTest.java
+++ src/test/java/tokenring/queue/QueueTokenRingTest.java
@@ -1,18 +1,31 @@
 package tokenring.queue;
 
 import org.junit.jupiter.api.Test;
+import tokenring.TokenRing;
+import tokenring.single.SingleTokenRing;
 
 import java.util.Arrays;
 
 class QueueTokenRingTest {
 	@Test
-	public void test() throws InterruptedException {
-		QueueTokenRing tokenRing = new QueueTokenRing(4, 10, x -> { });
-		tokenRing.sendTokens(38);
+	public void queueTest() throws InterruptedException {
+		QueueTokenRing tokenRing = new QueueTokenRing(4, 1, x -> { });
+		tokenRing.sendTokens(2);
 		tokenRing.start();
 		Thread.sleep(15000);
 		tokenRing.stop();
-		System.out.println(Arrays.toString(tokenRing.getLatencies()));
-		System.out.println(Arrays.toString(tokenRing.getThroughput()));
+		System.out.println("queue: " + Arrays.toString(tokenRing.getLatencies()));
+		System.out.println("queue: " + Arrays.toString(tokenRing.getThroughput()));
+	}
+
+	@Test
+	public void singleTest() throws InterruptedException {
+		TokenRing tokenRing = new SingleTokenRing(4, x -> { });
+		tokenRing.sendTokens(2);
+		tokenRing.start();
+		Thread.sleep(15000);
+		tokenRing.stop();
+		System.out.println("single: " + Arrays.toString(tokenRing.getLatencies()));
+		System.out.println("single: " + Arrays.toString(tokenRing.getThroughput()));
 	}
 }
